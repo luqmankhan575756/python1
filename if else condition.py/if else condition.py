4abccbeb411257ea640754fb1fba789eef1063b1
Luqman Khan <luqmankhan575756@gmail.com>
2025-11-04 19:53:33 +0500

    if else finist=h

---
+++ if else condition.py/if else condition.py
@@ -0,0 +1,16 @@
+x = 68
+if x < 0 or x > 100:
+    print ("Incorrect value.")
+elif x >= 50 and x < 60:
+    print ("E Grade.")
+
+elif x >= 60 and x < 70:
+    print ("D Grade.")
+
+elif x >= 70 and x < 80:
+    print ("C Grade.")
+
+elif x >= 80 and x < 90:
+    print ("B Grade.")
+else:
+    print ("A Grade")
