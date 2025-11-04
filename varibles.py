c6684081ff8d86a698f65c3c6b7ed9b523c35045
Luqman Khan <luqmankhan575756@gmail.com>
2025-11-04 13:08:46 +0500

    initial commit

---
+++ varibles.py
@@ -0,0 +1,25 @@
+# String variable
+student_name = "luqman khan"
+# string variable
+age = 21
+# Float variable
+height = 6.2
+# Boolean variable
+is_student = True
+# List variable
+marks = [85, 90, 78]
+# Tuple variable
+subjects = ("Math", "Python", "AI")
+
+student_info = {
+    "name" : student_name,
+    "age" : age,
+    "is_student" : is_student
+}
+
+_total_score = sum(marks)
+
+
+print( student_name,age, height, is_student)
+print(marks, subjects)
+print(student_info)
